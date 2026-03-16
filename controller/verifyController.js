98d9e6d0e944cebcd1a8968deb38f403dfa28f30
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-03-16 14:21:00 +0100

    Implementing new controller for email logic

---
+++ controller/verifyController.js
@@ -0,0 +1,42 @@
+import { getUser, resendMailVerification } from "../services/api.js";
+
+const verifyBlock = document.querySelector(".verifyBlock");
+
+if (verifyBlock) {
+	const verifyBtn = verifyBlock.querySelector("#resendBtn");
+
+	if (verifyBtn) {
+		try {
+			verifyBtn.addEventListener("click", async (e) => {
+				e.preventDefault();
+				const response = await resendMailVerification();
+				const result = await response.json();
+				verifyBtn.disabled = true;
+				verifyBtn.innerHTML = result.message;
+				return;
+			});
+		} catch (err) {
+			console.error(err);
+		}
+	}
+}
+
+async function checkVerification() {
+	try {
+		const response = await getUser();
+
+		if (!response.ok) {
+			throw new Error("Invalid or expired token");
+		}
+
+		const user = await response.json();
+
+		if (user.email_verified_at !== null) {
+			window.location.href = "/index.html";
+		}
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+checkVerification();
