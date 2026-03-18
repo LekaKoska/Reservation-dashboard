3f277a95767f8af8a2daa5fe04376deb434bc200
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-03-18 13:37:18 +0100

    Implementig logic for adding guests

--- controller/handleTable.js
+++ controller/handleTable.js
@@ -1,15 +1,22 @@
 import { reservations } from "../model/tables.js";
-import { renderTimeSlots } from "../view/renderTimes.js";
+import { tableReservation } from "./tableReservation.js";
 
 export function handleTableClick(event, state) {
-	const isFree = event.target.classList.contains("free");
-	const timesContainer = state.reservationBox.querySelector(".times");
+	const isFree = event.target.classList.contains("available");
 	if (isFree) {
 		const tableId = event.target.dataset.id;
-		state.selectedTable = reservations.find((table) => table.id == tableId);
-		state.timeSlots = state.selectedTable.timeSlots;
-		renderTimeSlots(state.timeSlots, timesContainer);
+		state.selectedTable = reservations.find(
+			(table) => table.table_id == tableId,
+		);
 
-		state.reservationBox.classList.remove("modal-hidden");
+		state.preOrder.classList.remove("modal-hidden");
+		document
+			.querySelector("#reservation-form")
+			.addEventListener("submit", async (e) => {
+				e.preventDefault();
+				await tableReservation(state);
+				state.preOrder.classList.add("modal-hidden");
+				state.selectedTable = null;
+			});
 	}
 }
